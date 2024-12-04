6963c774eddaf724ae18cc4984b05d6169b64f71
Jack Kalayjian <jack.kalayjian@revolve.com>
2024-12-04 13:39:26 -0800

    Vercel

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,23 @@
-import { db } from '@vercel/postgres';
+import { db } from "@vercel/postgres";
 
 const client = await db.connect();
 
 async function listInvoices() {
-  const result = await client.sql`
-  SELECT invoices.amount, customers.name
-  FROM invoices
-  JOIN customers ON invoices.customer_id = customers.id
-  WHERE invoices.amount = 666;
+	const data = await client.sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
   `;
-  return result.rows; // Return the rows containing invoice data
+
+	return data.rows;
 }
 
 export async function GET() {
-  try {
-    await client.sql`BEGIN`;
-    const invoices = await listInvoices();
-    await client.sql`COMMIT`;
 
-    return Response.json(invoices); // Return the actual invoice data
+  try {
+  	return Response.json(await listInvoices());
   } catch (error) {
-    await client.sql`ROLLBACK`;
-    return Response.json({ error }, { status: 500 });
+  	return Response.json({ error }, { status: 500 });
   }
 }
